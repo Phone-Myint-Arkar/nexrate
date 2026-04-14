983478ea646d873cf89ffb4b118a3ef0c2187cdc
“Phone-Myint-Arkar” <aarkar215@gmail.com>
2026-04-14 10:40:32 +0700

    v0.52

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def convert_api():
     
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 source venv/bin/activate
+lsof -ti:5001 | xargs kill -9
+sleep 1
 python app.py &
 sleep 2
 open index.html
+wait
