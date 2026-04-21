29d52824812521f1032c0580668dacab614e31ab
“Phone-Myint-Arkar” <aarkar215@gmail.com>
2026-04-21 11:02:56 +0700

    new start command

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 source venv/bin/activate
-lsof -ti:5001 | xargs kill -9
+lsof -ti:5001 | xargs kill -9 2>/dev/null
 sleep 1
 python app.py &
-sleep 2
+sleep 3
 open index.html
+trap "lsof -ti:5001 | xargs kill -9 2>/dev/null; exit" INT TERM EXIT
 wait
